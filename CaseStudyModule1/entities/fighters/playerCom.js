fa85ad8abeff258f9496137c868c3376caa74025
wjnzero <chuckieakenzzz@gmai.com>
2022-07-28 15:50:14 +0700

    fix

--- CaseStudyModule1/entities/fighters/playerCom.js
+++ CaseStudyModule1/entities/fighters/playerCom.js
@@ -1,8 +1,16 @@
-import {Fighter} from "./fighter.js";
+import { Fighter } from "./fighter.js";
 
 export class PlayerCom extends Fighter{
     constructor(x,y,vanToc) {
         super("playerCom",x,y,vanToc);
-        this.image=document.querySelector('img[alt="rock"]')
+        this.image=document.querySelector('img[alt="rock"]');
+        this.frames=new Map([
+            ['forwards-1',[74,23,70,50]],
+            ['forwards-2',[4,23,70,70]],
+            ['forwards-3',[230,25,69,50]],
+            ['forwards-4',[298,20,50,50]],
+            ['forwards-5',[348,20,60,50]],
+            ['forwards-6',[612,113,11,50]],
+        ]);
     }
 }
--- CaseStudyModule1/src/main.js
+++ CaseStudyModule1/src/main.js
@@ -7,7 +7,7 @@ const gameView = {
   WIDTH: 384, HEIGHT:224,
 };
 //khai báo att load page
-window.onload = function (){
+window.addEventListener("load",function (){
     const canvasGame=document.querySelector("canvas");
     const context=canvasGame.getContext('2d');
 
@@ -15,22 +15,34 @@ window.onload = function (){
     canvasGame.height=gameView.HEIGHT;
 
     const player=new Player(80,110,1);
-    const playerCom=new PlayerCom(80,110,-1);
+    const playerCom=new PlayerCom(200,145,-1);
     const stage=new Stage();
 
+    let frameTime={
+        previous:0,
+        secondsPassed:0,
+    };
+
+
     //hàm
-    function frame() {
-        player.update(context);
-        playerCom.update(context)
+    function frame(time) {
+        window.requestAnimationFrame(frame);
+
+        frameTime={
+            secondsPassed: (time-frameTime.previous)/50,
+            previous: time,
+        };
+        player.update(frameTime,context);
+        playerCom.update(frameTime,context);
 
         stage.draw(context);
         player.draw(context);
         playerCom.draw(context);
 
-        window.requestAnimationFrame(frame);
+
     }
     //hàm frame gọi lại
     window.requestAnimationFrame(frame)
 
     console.log("ready for serv!!");
-}
+})
