614fab6b141a42e46fc158e9264ef170a7e61357
hung <108741805+wjnzero@users.noreply.github.com>
2022-07-29 15:00:23 +0700

    Delete CaseStudyModule1 directory

--- CaseStudyModule1/entities/stage.js
+++
@@ -1,9 +0,0 @@
-export class Stage{
-    constructor() {
-        this.image=document.querySelector('img[alt="background"]');
-    }
-    draw(context){
-        context.drawImage(this.image,0,0);
-    }
-}
-
